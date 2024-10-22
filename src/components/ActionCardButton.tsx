13135376340dcd395398e0f1e7573bd97b8cb4ee
Diego Rodriguez <alejandro27062@gmail.com>
2024-10-21 20:27:44 -0500

    fix: refactor CardItem

--- src/components/ActionCardButton.tsx
+++ src/components/ActionCardButton.tsx
@@ -12,7 +12,7 @@ function ActionCardButton({ count, icon, flexDirection = "row", liked, onClick}:
   const bgIcon = liked ? "bg-green-400" : "bg-gray-400"
   return (
     <button onClick={onClick} className={`p-2 cursor-pointer border sm:border-gray-200 md:border-0 flex justify-center md:text-white items-center gap-3 md:gap-1 md:flex-col-reverse ${flexDirection}`}>
-      <p>{count}</p>
+      {count}
       <div className={`rounded-full p-1 text-white ${bgIcon} grid place-items-center`}>
       <FeatherIcon icon={icon} size={15} />
       </div>
--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -29,30 +29,17 @@ function CardItem({ product }: Props) {
   }
 
   return (
-    <article className="card--item border border-gray-200 shadow-sm relative overflow-hidden">
+    <article className="grid card--item border border-gray-200 shadow-sm relative overflow-hidden">
       <div className="bg-white absolute pb-6 pt-3 pl-2 w-[110px] h-[80px] triangle-clip-path">
       <p className="text-sm">{new Intl.NumberFormat("de-DE", {style: "currency", currency: "EUR"}).format(product.price)}</p>
       </div>
-      <div className="card--item__actions absolute w-full h-full bg-gradient-to-b from-gray-900 via-transparent hidden justify-start flex-col items-end">
-        <ActionCardButton
-          onClick={postLike}
-          icon="thumbs-up"
-          count={likes}
-          liked={liked}
-        />
-        <ActionCardButton 
-          icon="refresh-ccw"
-          count={0}
-          flexDirection="row-reverse"
-        />
-      </div>
-      <img src={image} alt={product.title} className="w-full object-cover aspect-square" />
-        <div className="bg-white text-center h-full">
+      <img src={image} alt={product.title} className="w-full h-full object-cover aspect-square" />
+        <div className="bg-white text-center">
           <div className="py-3">
             <p className="font-semibold text-lg uppercase">{product.title}</p>
             <p className="text-sm">By <strong>{product.author}</strong></p>
           </div>
-          <div className="grid grid-cols-2 justify-between md:hidden">
+          <div className="grid md:hidden grid-cols-2 justify-between card--item__actions md:absolute w-full md:top-0 md:h-full md:bg-gradient-to-b from-gray-900 via-transparent md:justify-start flex-col items-end">
             <ActionCardButton
               onClick={postLike}
               icon="thumbs-up"
